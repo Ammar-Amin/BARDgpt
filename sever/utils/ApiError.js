d611adb73c4e860eba911a43a75b55fcd2b2052d
Ammar-Amin <ammaramin5253@gmail.com>
2024-06-29 12:57:56 +0530

    Add utility classes ApiResponse, ApiError, and wrapAsync function for standardized API responses and error handling

---
+++ sever/utils/ApiError.js
@@ -0,0 +1,12 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong"
+    ) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.success = false
+    }
+}
+
+export { ApiError }
